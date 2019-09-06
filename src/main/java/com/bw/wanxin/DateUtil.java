0469e2de8996d3f08ada2dcf75c64fb8787c9bc5
BW05WANXIN <2293152624@qq.com>
2019-09-06 14:16:02 +0800

    修改后日期工具类

--- src/main/java/com/bw/wanxin/DateUtil.java
+++ src/main/java/com/bw/wanxin/DateUtil.java
@@ -3,10 +3,41 @@
  */
 package com.bw.wanxin;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * @author 926474
  *
  */
 public class DateUtil {
+	
+	public static int getAge(Date birthday) {
+		// 日历类 calendar
+		Calendar instance = Calendar.getInstance();
+		instance.setTime(birthday);
+		
+		// 得到出生的年月日
+		int birthYear = instance.get(Calendar.YEAR);
+		int birthMonth = instance.get(Calendar.MONTH);
+		int birthDate = instance.get(Calendar.DAY_OF_MONTH);
+		
+		// 获取当前的年月日
+		instance.setTime(new Date());
+		int currentYear = instance.get(Calendar.YEAR);
+		int currentMonth = instance.get(Calendar.MONTH);
+		int currentDate = instance.get(Calendar.DAY_OF_MONTH);
+		
+		// 开始计算
+		int age = currentYear-birthYear;
+		if (currentMonth<birthMonth) {
+			age--;
+		}else if(currentMonth==birthMonth && currentDate<birthDate) {
+			age--;
+		}
+		
+		return age;
+		
+	}
 
 }
