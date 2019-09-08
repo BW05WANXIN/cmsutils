086ba46d06fd9db91cac61d0ec24e5615aa55044
BW05WANXIN <2293152624@qq.com>
2019-09-08 19:06:44 +0800

    9.8 19:06 修改提交

---
+++ src/main/java/com/bw/wanxin/StreamUtils.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.bw.wanxin;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * @author 926474
+ *
+ */
+public class StreamUtils {
+	
+	/**
+	 * 关闭流
+	 * @param closeables
+	 * @throws IOException
+	 */
+	public static void closeStream(Closeable ...closeables) throws IOException {
+		for (Closeable closeable : closeables) {
+			closeable.close();
+		}
+	}
+	
+	public static void copyStream(InputStream is,OutputStream os) throws IOException {
+		byte b[]=new byte[1024];
+		while(is.read(b)>0) {
+			os.write(b);
+		}
+		os.flush();
+	}
+
+}
---
+++ src/main/java/com/bw/wanxin/test/TestClass.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.bw.wanxin.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 926474
+ *
+ */
+@Service
+public class TestClass {
+	
+	
+
+}
